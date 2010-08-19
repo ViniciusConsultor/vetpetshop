05ba334c13fb13f9d3e33987c42bb70a118a2fa5
rafael.marateo <rafael.marateo@6b5ef840-633f-fa3f-50a1-21cfc8f2aacc>
2010-08-19 02:47:07 +0000

    Liberado por Rafael

---
+++  vetpetshop --username abecaferreira/Implementação/Solution/Entidade/Animal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlTypes;
+
+namespace Entidade
+{
+    class Animal
+    {
+        public string Nome { get; set; }
+        public int TipoAnimal { get; set; }
+        public decimal Peso { get; set; }
+        public string Raca { get; set; }
+    }
+}
