fe1fae7cb6c4559b1bf32496a7faded0c770cd85
rafael.marateo <rafael.marateo@6b5ef840-633f-fa3f-50a1-21cfc8f2aacc>
2010-08-27 04:21:43 +0000

    Rafael

--- trunk/ vetpetshop --username abecaferreira/Implementação/Solution/WebUI/ManterCadastroCliente.aspx.designer.cs
+++ trunk/ vetpetshop --username abecaferreira/Implementação/Solution/WebUI/ManterCadastroCliente.aspx.designer.cs
@@ -247,6 +247,15 @@ namespace WebUI {
         /// </remarks>
         protected global::AjaxControlToolkit.ModalPopupExtender ModalPopupExtender1;
         
+        // <summary>
+        /// ToolkitScriptManager1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::AjaxControlToolkit.ToolkitScriptManager ToolkitScriptManager1;
+
         /// <summary>
         /// Panel1 control.
         /// </summary>
